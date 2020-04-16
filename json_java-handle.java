78dcb61314fdc2a5d9b225392c3e433229bc8d5e
mani1893 <venkatmani1893@gmail.com>
2020-04-17 00:16:29 +0530

    Update json_java-handle.java

--- json_java-handle.java
+++ json_java-handle.java
@@ -1,1 +1,28 @@
-Hi
+package json_writter;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("ID", "1");
+	      jsonObject.put("First_Name", "Shikhar");
+	      jsonObject.put("Last_Name", "Dhawan");
+	      jsonObject.put("Date_Of_Birth", "1981-12-05");
+	      jsonObject.put("Place_Of_Birth", "Delhi");
+	      jsonObject.put("Country", "India");
+	      try {
+	         FileWriter file = new FileWriter("C:\\Users\\venka\\eclipse-workspace\\output.json");
+	         file.write(jsonObject.toJSONString());
+	         file.close();
+	      } catch (IOException e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      }
+	      System.out.println("JSON file created: "+jsonObject);
+
+	}
+
+}
